33a213443b62cb66a7b5f7a17d6bf16b8b190132
Beau Dobbin <beau@beaunus.com>
2020-02-19 21:28:59 +0900

    refactor: readability improvements

--- src/main/java/com/beaunus/Sort.java
+++ src/main/java/com/beaunus/Sort.java
@@ -1,48 +1,55 @@
 package com.beaunus;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Sort {
 
   public static <T extends Comparable<T>> void quicksort(T[] array) {
-    quicksort(array, 0, array.length - 1);
+    Random random = new Random();
+    System.out.println(Arrays.toString(array));
+    quicksort(array, random, 0, array.length - 1);
+    System.out.println(Arrays.toString(array));
   }
 
-  private static <T extends Comparable<T>> void quicksort(T[] array, int lo, int hi) {
+  private static <T extends Comparable<T>> void quicksort(T[] array, Random random, int lo,
+      int hi) {
     if (hi <= lo) {
       return;
     }
-    Random random = new Random();
-    int pivotIndex = lo + random.nextInt(hi - lo);
-    swap(array, lo, pivotIndex);
+
+    int randomIndex = lo + random.nextInt(hi - lo);
+    swap(array, lo, randomIndex);
+
     int p = partition(array, lo, hi);
-    quicksort(array, lo, p - 1);
-    quicksort(array, p + 1, hi);
+    quicksort(array, random, lo, p - 1);
+    quicksort(array, random, p + 1, hi);
   }
 
   private static <T extends Comparable<T>> int partition(T[] array, int lo, int hi) {
     int i = lo;
     int j = hi + 1;
-    T v = array[lo];
-    
+
+    T pivot = array[lo];
+
     while (true) {
-      while (array[++i].compareTo(v) < 0) {
-        if (i == hi) {
-          break;
-        }
-      }
-      while (v.compareTo(array[--j]) < 0) {
-        if (j == lo) {
-          break;
-        }
-      }
+      do {
+        ++i;
+      } while (array[i].compareTo(pivot) <= 0 && i < hi);
+
+      do {
+        --j;
+      } while (array[j].compareTo(pivot) >= 0 && j > lo);
+
       if (i >= j) {
         break;
       }
       swap(array, i, j);
     }
+
     swap(array, lo, j);
     return j;
+
   }
 
   private static void swap(Comparable<?>[] array, int i, int j) {
