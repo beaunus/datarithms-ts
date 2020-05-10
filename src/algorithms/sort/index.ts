8674669a0ed78dfe89ee548509037e7c742a5f55
Beau Dobbin <beau@beaunus.com>
2020-05-10 19:06:58 +0900

    fix quicksort3Way

--- src/algorithms/sort/index.ts
+++ src/algorithms/sort/index.ts
@@ -1,15 +1,17 @@
 /**
  * Sort in place and return the given array sorted with the Quicksort algorithm.
  *
+ * Adapted from https://algs4.cs.princeton.edu/code/javadoc/edu/princeton/cs/algs4/Quick3way.html
+ *
  * @param array
  */
-export function quicksort(
+export function quicksort3Way(
   array: Array<number>,
   lo = 0,
   hi = array.length - 1
 ): Array<number> {
   if (lo >= hi) return array;
-  const pivotIndex = Math.floor(Math.random() * (1 + hi - lo));
+  const pivotIndex = Math.floor(Math.random() * (hi - lo) + lo);
   [array[lo], array[pivotIndex]] = [array[pivotIndex], array[lo]];
 
   let lt = lo;
@@ -27,8 +29,7 @@ export function quicksort(
       --gt;
     } else ++i;
 
-  quicksort(array, lo, lt - 1);
-  quicksort(array, gt + 1, hi);
-
-  return array.sort();
+  quicksort3Way(array, lo, lt - 1);
+  quicksort3Way(array, gt + 1, hi);
+  return array;
 }
