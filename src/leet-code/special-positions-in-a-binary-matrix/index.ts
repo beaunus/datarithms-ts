6da406aa6cb5952ab235f686ef98534fb52395b4
Beau Dobbin <beau@beaunus.com>
2021-07-05 23:00:09 +0900

    refactor: remove brackets

--- src/leet-code/special-positions-in-a-binary-matrix/index.ts
+++ src/leet-code/special-positions-in-a-binary-matrix/index.ts
@@ -2,9 +2,9 @@ export function numSpecial(matrix: number[][]): number {
   const numOnesByRowIndex = Array(matrix.length).fill(0);
   const numOnesByColIndex = Array(matrix[0].length).fill(0);
   for (let i = 0; i < numOnesByRowIndex.length; ++i)
-    for (let j = 0; j < numOnesByColIndex.length; ++j) {
+    for (let j = 0; j < numOnesByColIndex.length; ++j)
       if (matrix[i][j]) ++numOnesByRowIndex[i], ++numOnesByColIndex[j];
-    }
+
   let result = 0;
   for (let i = 0; i < numOnesByRowIndex.length; ++i)
     if (numOnesByRowIndex[i] === 1)
