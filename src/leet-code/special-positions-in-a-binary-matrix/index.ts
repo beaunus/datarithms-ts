3b8c8ea0a0d21d020f31f93f1fb5a66f0b145506
Beau Dobbin <beau@beaunus.com>
2021-07-05 22:58:53 +0900

    refactor: rename matrix

--- src/leet-code/special-positions-in-a-binary-matrix/index.ts
+++ src/leet-code/special-positions-in-a-binary-matrix/index.ts
@@ -1,14 +1,14 @@
-export function numSpecial(mat: number[][]): number {
-  const numOnesByRowIndex = Array(mat.length).fill(0);
-  const numOnesByColIndex = Array(mat[0].length).fill(0);
+export function numSpecial(matrix: number[][]): number {
+  const numOnesByRowIndex = Array(matrix.length).fill(0);
+  const numOnesByColIndex = Array(matrix[0].length).fill(0);
   for (let i = 0; i < numOnesByRowIndex.length; ++i)
     for (let j = 0; j < numOnesByColIndex.length; ++j) {
-      if (mat[i][j]) ++numOnesByRowIndex[i], ++numOnesByColIndex[j];
+      if (matrix[i][j]) ++numOnesByRowIndex[i], ++numOnesByColIndex[j];
     }
   let res = 0;
   for (let i = 0; i < numOnesByRowIndex.length; ++i)
     if (numOnesByRowIndex[i] === 1)
       for (let j = 0; j < numOnesByColIndex.length; ++j)
-        if (mat[i][j] && numOnesByColIndex[j] === 1) ++res;
+        if (matrix[i][j] && numOnesByColIndex[j] === 1) ++res;
   return res;
 }
