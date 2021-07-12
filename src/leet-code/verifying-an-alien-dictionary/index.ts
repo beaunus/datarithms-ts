b54595152247a76a0ce4c588c26dc2f1e8a2087c
Beau Dobbin <beau@beaunus.com>
2021-07-12 22:52:21 +0900

    refactor: improve readability

--- src/leet-code/verifying-an-alien-dictionary/index.ts
+++ src/leet-code/verifying-an-alien-dictionary/index.ts
@@ -1,24 +1,21 @@
 function isAlienSorted(words: string[], order: string): boolean {
-  const positionByCharacter = order
+  const positionByChar = order
     .split("")
     .reduce<Record<string, number>>(
       (acc, cur, index) => ({ ...acc, [cur]: index }),
       {}
     );
   for (let i = 1; i < words.length; ++i) {
-    const shortestLength = Math.min(words[i].length, words[i - 1].length);
-    let isLeftWordLower = false;
-    for (let j = 0; !isLeftWordLower && j <= shortestLength; ++j) {
-      if (
-        positionByCharacter[words[i - 1][j]] < positionByCharacter[words[i][j]]
-      )
-        isLeftWordLower = true;
+    const word = words[i];
+    const prevWord = words[i - 1];
+    const shortestLength = Math.min(word.length, prevWord.length);
+    for (let j = 0; j <= shortestLength; ++j)
+      if (positionByChar[prevWord[j]] < positionByChar[word[j]]) break;
       else if (
-        (!words[i][j] && words[i - 1][j]) ||
-        positionByCharacter[words[i][j]] < positionByCharacter[words[i - 1][j]]
+        (!word[j] && prevWord[j]) ||
+        positionByChar[word[j]] < positionByChar[prevWord[j]]
       )
         return false;
-    }
   }
   return true;
 }
