21d36b1bb47ca3697bf114720ea76142dee86088
Beau Dobbin <beau@beaunus.com>
2021-07-12 22:15:28 +0900

    refactor: reduce conditions

--- src/leet-code/crawler-log-folder/index.ts
+++ src/leet-code/crawler-log-folder/index.ts
@@ -1,9 +1,8 @@
 function minOperations(logs: string[]): number {
   let result = 0;
   for (const log of logs) {
-    if (log === "./") continue;
-    if (log === "../") result = result ? result - 1 : 0;
-    else ++result;
+    if (log[0] !== ".") ++result;
+    if (log[1] === ".") result = result ? result - 1 : 0;
   }
   return Math.max(result, 0);
 }
