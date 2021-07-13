bfe9ee831fce5219c467f2f8ce06326023dc3ba4
Beau Dobbin <beau@beaunus.com>
2021-07-13 22:39:50 +0900

    refactor: inline .length

--- src/leet-code/implement-strstr/index.ts
+++ src/leet-code/implement-strstr/index.ts
@@ -11,9 +11,8 @@ function strStr(haystack: string, needle: string): number {
 }
 
 function kmpProcess(needle: string): number[] {
-  const n = needle.length;
-  const lps = Array(n).fill(0);
-  for (let i = 1, len = 0; i < n; )
+  const lps = Array(needle.length).fill(0);
+  for (let i = 1, len = 0; i < needle.length; )
     if (needle[i] == needle[len]) lps[i++] = ++len;
     else if (len) len = lps[len - 1];
     else lps[i++] = 0;
