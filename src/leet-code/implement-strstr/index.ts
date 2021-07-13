ca0edf4c882ef3ec4bf9ccf2b48ae1927d1dec56
Beau Dobbin <beau@beaunus.com>
2021-07-13 22:39:24 +0900

    refactor: whitespace

--- src/leet-code/implement-strstr/index.ts
+++ src/leet-code/implement-strstr/index.ts
@@ -1,18 +1,11 @@
 function strStr(haystack: string, needle: string): number {
-  if (!needle.length) {
-    return 0;
-  }
+  if (!needle.length) return 0;
   const lps = kmpProcess(needle);
   for (let i = 0, j = 0; i < haystack.length; ) {
-    if (haystack[i] == needle[j]) {
-      i++, j++;
-    }
-    if (j == needle.length) {
-      return i - j;
-    }
-    if (i < haystack.length && haystack[i] != needle[j]) {
+    if (haystack[i] == needle[j]) i++, j++;
+    if (j == needle.length) return i - j;
+    if (i < haystack.length && haystack[i] != needle[j])
       j ? (j = lps[j - 1]) : i++;
-    }
   }
   return -1;
 }
@@ -20,15 +13,10 @@ function strStr(haystack: string, needle: string): number {
 function kmpProcess(needle: string): number[] {
   const n = needle.length;
   const lps = Array(n).fill(0);
-  for (let i = 1, len = 0; i < n; ) {
-    if (needle[i] == needle[len]) {
-      lps[i++] = ++len;
-    } else if (len) {
-      len = lps[len - 1];
-    } else {
-      lps[i++] = 0;
-    }
-  }
+  for (let i = 1, len = 0; i < n; )
+    if (needle[i] == needle[len]) lps[i++] = ++len;
+    else if (len) len = lps[len - 1];
+    else lps[i++] = 0;
   return lps;
 }
 
