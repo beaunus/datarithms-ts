f5fad8ed007237dad346e2e57496cbd3becf64b7
Beau Dobbin <beau@beaunus.com>
2021-07-13 23:09:36 +0900

    feature: implement wikipedia KMP

--- src/leet-code/implement-strstr/index.ts
+++ src/leet-code/implement-strstr/index.ts
@@ -1,22 +1,26 @@
-function strStr(haystack: string, needle: string): number {
-  if (!needle.length) return 0;
-  const lps = kmpProcess(needle);
-  for (let i = 0, j = 0; i < haystack.length; ) {
-    if (haystack[i] === needle[j]) i++, j++;
-    if (j === needle.length) return i - j;
-    if (i < haystack.length && haystack[i] !== needle[j])
-      j ? (j = lps[j - 1]) : i++;
-  }
+function strStr(S: string, W: string): number {
+  if (!W.length) return 0;
+  const T = kmpProcess(W);
+  for (let j = 0, k = 0; j < S.length; )
+    if (W[k] === S[j]) {
+      j++, k++;
+      if (k === W.length) return j - k;
+    } else {
+      k = T[k];
+      if (k < 0) j++, k++;
+    }
   return -1;
 }
 
-function kmpProcess(needle: string): number[] {
-  const lps = Array(needle.length).fill(0);
-  for (let i = 1, len = 0; i < needle.length; )
-    if (needle[i] === needle[len]) lps[i++] = ++len;
-    else if (len) len = lps[len - 1];
-    else lps[i++] = 0;
-  return lps;
+function kmpProcess(W: string): number[] {
+  const T = Array(W.length).fill(-1);
+  for (let pos = 1, cnd = 0; pos < W.length; ++pos, ++cnd)
+    if (W[pos] === W[cnd]) T[pos] = T[cnd];
+    else {
+      T[pos] = cnd;
+      while (cnd >= 0 && W[pos] !== W[cnd]) cnd = T[cnd];
+    }
+  return T;
 }
 
 export { strStr };
