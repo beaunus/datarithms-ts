d03d4aaed22fd3f473664404e8dfc12ef51e79e7
Beau Dobbin <beau@beaunus.com>
2021-07-13 22:36:42 +0900

    fix: use kmp

--- src/leet-code/implement-strstr/index.ts
+++ src/leet-code/implement-strstr/index.ts
@@ -1,14 +1,37 @@
 function strStr(haystack: string, needle: string): number {
-  for (let i = 0; i < haystack.length; ++i) {
-    let numMatches = 0;
-    while (
-      numMatches < needle.length &&
-      needle[numMatches] === haystack[i + numMatches]
-    )
-      ++numMatches;
-    if (numMatches === needle.length) return i;
+  const m = haystack.length;
+  const n = needle.length;
+  if (!n) {
+    return 0;
   }
-  return needle.length ? -1 : 0;
+  const lps = kmpProcess(needle);
+  for (let i = 0, j = 0; i < m; ) {
+    if (haystack[i] == needle[j]) {
+      i++, j++;
+    }
+    if (j == n) {
+      return i - j;
+    }
+    if (i < m && haystack[i] != needle[j]) {
+      j ? (j = lps[j - 1]) : i++;
+    }
+  }
+  return -1;
+}
+
+function kmpProcess(needle: string): number[] {
+  const n = needle.length;
+  const lps = Array(n).fill(0);
+  for (let i = 1, len = 0; i < n; ) {
+    if (needle[i] == needle[len]) {
+      lps[i++] = ++len;
+    } else if (len) {
+      len = lps[len - 1];
+    } else {
+      lps[i++] = 0;
+    }
+  }
+  return lps;
 }
 
 export { strStr };
