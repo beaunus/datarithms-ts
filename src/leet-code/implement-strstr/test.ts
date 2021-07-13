bb18b54f0c3871ea07d37a9f663101d3b3e33a8a
Beau Dobbin <beau@beaunus.com>
2021-07-13 22:50:42 +0900

    refactor: use spread

--- src/leet-code/implement-strstr/test.ts
+++ src/leet-code/implement-strstr/test.ts
@@ -10,7 +10,7 @@ const TEST_CASES: { args: [string, string]; expected: any }[] = [
 describe("implement-strstr", () => {
   TEST_CASES.forEach(({ args, expected }) =>
     it(`should return ${expected} for ${args.join(", ")}`, () =>
-      expect(strStr.apply(null, args)).toBe(expected))
+      expect(strStr(...args)).toBe(expected))
   );
   it("should return the correct result for long strings", () => {
     const haystack = Array(100000).fill("a").concat(["b"]).join("");
