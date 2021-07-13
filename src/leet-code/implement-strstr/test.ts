ab868e0680d9adca6133003bd52d5eddd26a8af1
Beau Dobbin <beau@beaunus.com>
2021-07-13 21:57:15 +0900

    refactor: tests

--- src/leet-code/implement-strstr/test.ts
+++ src/leet-code/implement-strstr/test.ts
@@ -1,10 +1,15 @@
 import { strStr } from "./index";
 
+const TEST_CASES: { args: [string, string]; expected: any }[] = [
+  { args: ["hello", "ll"], expected: 2 },
+  { args: ["aaaaa", "bba"], expected: -1 },
+  { args: ["", ""], expected: 0 },
+  { args: ["", "a"], expected: -1 },
+];
+
 describe("implement-strstr", () => {
-  it("should pass the test cases", () => {
-    expect(strStr("hello", "ll")).toBe(2);
-    expect(strStr("aaaaa", "bba")).toBe(-1);
-    expect(strStr("", "")).toBe(0);
-    expect(strStr("", "a")).toBe(-1);
-  });
+  TEST_CASES.forEach(({ args, expected }) =>
+    it(`should return ${expected} for ${args.join(", ")}`, () =>
+      expect(strStr.apply(null, args)).toBe(expected))
+  );
 });
